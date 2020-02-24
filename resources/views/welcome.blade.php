46c04f147711e415f6b6f89ab40653fc3393c7c2
StevenYangSX <yangshixin1206@icloud.com>
2020-02-23 22:21:42 -0800

    Made changed on welcom page link tags about assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
-    <script src="{{ asset('js/app.js')}}" defer></script>
+        <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js" defer></script>
 
     </head>
     <body>
